bd1a78fc8e63609ba82451703b970fa3df5c95ff
minjaejeon <minjaejeon0827@gmail.com>
2026-07-20 16:09:58 +0900

    feat: my-streamlit-demo/app.py streamlit 기능 구현

---
+++ my-streamlit-demo/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
+
+st.title("🚀 배포 테스트 앱")
+n = st.slider("데이터 포인트", 10, 200, 50)
+data = np.random.randn(n)
+fig = px.histogram(x=data, title="정규분포 샘플")
+st.plotly_chart(fig)
+st.success("배포 성공!")
